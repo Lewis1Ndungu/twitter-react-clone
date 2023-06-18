17ee44b824599f755ad8cecdf4b6ae2d358a2816
Lewis1Ndungu <lewis.ndungu@student.moringaschool.com>
2023-06-18 12:20:32 +0300

    Login functionality

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -2,11 +2,24 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Tweet from './components/Tweet';
 import TweetForm from './components/TweetForm';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
   const [tweets, setTweets] = useState([]);
 
-  const handleTweetSubmit = (username, content) => {
+  const handleLogin = (username) => {
+    setLoggedIn(true);
+    setUsername(username);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername('');
+  };
+
+  const handleTweetSubmit = (content) => {
     const newTweet = { username, content };
     setTweets((prevTweets) => [...prevTweets, newTweet]);
   };
@@ -14,16 +27,23 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <TweetForm onSubmit={handleTweetSubmit} />
-      <div className="tweet-list">
-        {tweets.map((tweet, index) => (
-          <Tweet
-            key={index}
-            username={tweet.username}
-            content={tweet.content}
-          />
-        ))}
-      </div>
+      {!loggedIn ? (
+        <LoginForm onLogin={handleLogin} />
+      ) : (
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+          <TweetForm onSubmit={handleTweetSubmit} />
+          <div className="tweet-list">
+            {tweets.map((tweet, index) => (
+              <Tweet
+                key={index}
+                username={tweet.username}
+                content={tweet.content}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
